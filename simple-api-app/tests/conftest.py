091c7b45f4dc89ad2b3d272ffbbccc0352df1a98
Boon Heng <tanboonheng@outlook.com>
2025-01-29 05:05:42 +0800

    fix import main issue

--- simple-api-app/tests/conftest.py
+++ simple-api-app/tests/conftest.py
@@ -2,12 +2,12 @@ import pytest
 from mongomock import MongoClient
 import mongomock
 import os
-from main import app
+from simple_api_app.main import app
 
 # Override the MongoDB client with mongomock
 @pytest.fixture(autouse=True)
 def mock_mongodb_connection(monkeypatch):
     mock_client = MongoClient()
-    monkeypatch.setattr("main.client", mock_client)
-    monkeypatch.setattr("main.db", mock_client.products_db)
+    monkeypatch.setattr("simple_api_app.main.client", mock_client)
+    monkeypatch.setattr("simple_api_app.main.db", mock_client.products_db)
     return mock_client 
